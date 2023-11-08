7769a42409315cbc4c0a3bccd44532329669893a
Teerut Srithongdee <teerutsr@gmail.com>
2023-11-09 02:22:18 +0700

    Add manifest name to VitePWA plugin configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+    plugins: [react(), VitePWA({ registerType: "autoUpdate", manifest: {
+        name: "Net Buffet",
+    } })],
 });
