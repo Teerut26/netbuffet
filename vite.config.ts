862cfcde0149248dfe055f05292bbe1fc891a769
Teerut Srithongdee <teerutsr@gmail.com>
2023-11-09 02:27:55 +0700

    Add short_name to PWA manifest.

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react(), VitePWA({ registerType: "autoUpdate", manifest: {
         name: "Net Buffet",
+        short_name: "Net Buffet",
     } })],
 });
